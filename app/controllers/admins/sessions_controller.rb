d22b10afab622ccfd139ee18842c911ceaec4762
Wong You Jing <wongyoujing118@gmail.com>
2017-02-02 21:53:26 +0800

    change admin login

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -16,7 +16,11 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || admin_dashboard_path
+  end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
