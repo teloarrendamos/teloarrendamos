bfb2edb39baf78702d3028ff9ab73e3e6d6d0ac1
Ignacio Palma <naxopalma@gmail.com>
2017-02-14 02:17:33 -0300

    few changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   #layout 'pages'
   def home
-  	@listings = Listing.order('created_at DESC').limit(5)  	
-  	@categories = Category.order('created_at DESC').limit(5)  	
+  	@listings = Listing.order('created_at DESC').limit(5)
+  	@categories = Category.order('created_at DESC').limit(5)
+    @total_listings = Listing.all.count
   end
 end
