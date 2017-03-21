e8a5adbba27f95d2ff67730e0c617906f098df6d
Ignacio Palma <naxopalma@gmail.com>
2017-03-21 19:11:48 -0300

    first responsive design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   #layout 'pages'
   def home
   	@listings = Listing.order('created_at DESC').limit(5)
-  	@categories = Category.order('created_at DESC').limit(5)
+  	@categories = Category.order('created_at DESC').limit(5)  	
     @total_listings = Listing.all.count
   end
 end
